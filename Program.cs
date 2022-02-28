4a6ee7829d3418495758eb9c934ce486fa050a11
Ali Paradise <alison.paradise@gmail.com>
2022-02-28 11:17:46 -0600

    phase 4

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
 ﻿using System;
 
+
+int secretNumber = 42;
+int guessesLeft = 4;
+int guessNumber = 1;
+
+while (guessNumber <= guessesLeft)
+{
+Console.WriteLine($"Your guess - {guessNumber}");
 Console.WriteLine("Please guess the secret number");
 
-string guess = Console.ReadLine();
+int guess=int.Parse(Console.ReadLine());
+if (guess==secretNumber){
+    Console.WriteLine("You guessed right!");
+    break;
+}
+else {
+    Console.WriteLine("Sorry, you are wrong");
+}
+guessNumber++;    
+}
 
-Console.WriteLine($"Your guess was {guess}.");
+
